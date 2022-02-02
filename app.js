bab924a9b3af3a428fc8b505ad76d59b0370e33c
Nitin goswami <nitingoswami@1900@gmail.com>
2022-02-02 14:56:18 +0530

    nik commit

--- app.js
+++ app.js
@@ -1,10 +1,27 @@
 var express = require("express");
 var path = require("path");
+var mysql = require('mysql');
 
 //var routes = require("./routes");
 
 var app = express();
 const port=3309
+
+
+var con = mysql.createConnection({
+  host: "localhost",
+  user: "nitin",
+  password: "nitin"
+});
+
+con.connect(function(err) {
+    if (err) throw err;
+    console.log("Connected!");
+    con.query("CREATE DATABASE mydb", function (err, result) {
+      if (err) throw err;
+      console.log("Database created");
+    });
+});
 
 // app.set("port",process.env.port//80);
 app.set("views", path.join(__dirname, "views"));
