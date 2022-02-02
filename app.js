6e3f01cc6f55f3f5268aa0fc2d05a486cf1a4a22
rekulapallisupriya <supriyareddy019@gmail.com>
2022-02-02 08:41:35 +0530

    near by restoo

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ var path = require("path");
 
 var app = express();
 
-app.set("port", process.env.PORT || 3003);
+app.set("port", process.env.PORT || 3330);
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
@@ -13,7 +13,7 @@ app.use("/", require("./routes/web"));
 app.use("/api", require("./routes/api"));
 
 
-app.listen(app.get("port"), function(){
+app.listen(app.get("port......"), function(){
     console.log("Server started on port......" + app.get("port"));
 })
 
