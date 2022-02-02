61e3c27f376cd7bee5430c738a240cbf8ae45f98
Nitin goswami <nitingoswami@1900@gmail.com>
2022-02-02 19:40:52 +0530

    nik commit 2

--- app.js
+++ app.js
@@ -1,35 +1,61 @@
 var express = require("express");
+var app = express();
 var path = require("path");
 var mysql = require('mysql');
+const bodyparser = require("body-parser")
+const session = require('express-session')
+const {v4: uuidv4} = require('uuid')
+const passport = require('passport')
+require('dotenv').config
+require('./passport-setup')
 
-//var routes = require("./routes");
+// const routes = require("./routes");
 
-var app = express();
-const port=3309
+const port=3300
 
+app.use(session({
+    secret: uuidv4(),
+    resave: false,
+    saveUninitialized: true
+}));
+// var con = mysql.createConnection({
+//   host: "localhost",
+//   user: "nitin",
+//   password: "nitin"
+// });
 
-var con = mysql.createConnection({
-  host: "localhost",
-  user: "nitin",
-  password: "nitin"
-});
-
-con.connect(function(err) {
-    if (err) throw err;
-    console.log("Connected!");
-    con.query("CREATE DATABASE mydb", function (err, result) {
-      if (err) throw err;
-      console.log("Database created");
-    });
-});
+// con.connect(function(err) {
+//     if (err) throw err;
+//     console.log("Connected!");
+//     // con.query("CREATE DATABASE mydb", function (err, result) {
+//     //   if (err) throw err;
+//     //   console.log("Database created");
+//     // });
+// });
 
 // app.set("port",process.env.port//80);
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
+app.use(passport.initialize())
+app.use(passport.session())
+
 app.use("/", require("./routes/web"));
 app.use("/api", require("./routes/api"));
 
+app.post("/login/", (req, res)=>{
+    // const {email, password} = req.body
+    console.log(req.body)
+
+    // if (req.body.email == credential.email && req.body.password == credential.password){
+    //     req.session.user = req.body.email
+    //     // res.redirect('/')
+    //     res.end('login success')
+    // }else{
+    //     res.end('invalid username and password')
+    // }
+    res.send(req.body)
+ });
 
 app.listen(port, ()=>{
     console.log(`Server listening at http://localhost:${port}`);
--- routes/web/home.js
+++ routes/web/home.js
@@ -1,4 +1,5 @@
 var express = require("express");
+const passport = require("passport");
 
 var router = express.Router();
 
@@ -16,13 +17,40 @@ router.get("/", function(req, res) {
     res.render("home/about");
  });
 
-router.get("/login", function(req, res){
+router.get("/login/", function(req, res){
     res.render("home/login")
  });
+router.get("/login", function(req, res){
+    res.render("home/login")
+});
 
-router.get("/signup", function(req, res){
-    res.render("home/signup")
- });
+
+
+const credential = {
+    email: "nitingoswami1900@gmail.com",
+    password: "shayna"
+}
+
+// router.post("/login/", (req, res)=>{
+//     // const {email, password} = req.body
+//     console.log(req)
+
+//     if (req.body.email == credential.email && req.body.password == credential.password){
+//         req.session.user = req.body.email
+//         // res.redirect('/')
+//         res.end('login success')
+//     }else{
+//         res.end('invalid username and password')
+//     }
+//  });
+
+router.get('/google', passport.authenticate('google', { scope: ['profile', 'email'] }));
+router.get('/google/callback', passport.authenticate('google', { failureRedirect: '/failed' }),
+  function(req, res) {
+    // Successful authentication, redirect home.
+    res.redirect('/home');
+  }
+);
 
 
  module.exports = router;
